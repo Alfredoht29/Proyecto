4ad286bfdb3e5bade4912957d892ca8813b6a6b8
Alfredoht <Emilioaperezvilla@outlook.com>
2022-12-07 19:36:03 -0600

    login

--- Proyecto/Registrar.php
+++ Proyecto/Registrar.php
@@ -1,10 +1,17 @@
 <?php
 include 'BdConnection.php';
-$mensaje="error";
+$retorno = array(
+    'exito' => false,
+);
 $post = empty($_POST) ? json_decode(file_get_contents('php://input'), true) : $_POST;
 $nickname=$post['nickname'];
 $password=$post['password'];
 $rol=$post['rol'];
 $insert="insert into usuarios values(null,'$nickname','$password','$rol',null)";
 $resultado=mysqli_query($conexion,$insert);
+if($resultado){
+$retorno['exito']=true;
+}
+header('Content-type: application/json');
+echo json_encode( $retorno);
 ?>
